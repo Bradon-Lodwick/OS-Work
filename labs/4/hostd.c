9b7b4270d8562912d678ea9e72d58b112e7bd6cc
ChazzyDango <Thomas.Reis@uoit.net>
2018-03-31 20:03:03 -0400

    Touchups on hostd.c

--- labs/4/hostd.c
+++ labs/4/hostd.c
@@ -39,96 +39,111 @@ int main(int argc, char *argv[])
 	
 	//TODO IMPORTANT: WHEN KILLING A PROCESS DECREASE RUN BY 1
 
-	struct res freeres;
+	struct resources freeres;
 	freeres.printer = 2;
-	freeres.scaner = 1;
+	freeres.scanner = 1;
 	freeres.modem = 1;
 	freeres.CD = 2;
 	memset(freeres.memory, 0, sizeof freeres.memory);
-
-	//TODO: FIGURE OUT HOW TO DECLARE QUEUES FOR Priority1, Priority2, and Priority3 and Realtime
 	
 	//puts all processes into the job dispatch list queue
 	int time = 0;
+	int memP = -1;
 	int run = lines;
+	node_t * tempProc;
 	while(run > 0)
 	{
 		for (i=0;i < lines;i++)
 		{
-			
+			//pushes the processes
 			if (processes[i].arrivalTime == time)
 			{
 				//put that shit in the appropriate queue
 				//this means either the realtime queue
 				//or the user job queue
+				if (processes[i].priority == 0)
+				{
+					push(0, processes[i]);
+				}
+				else
+				{
+					push(4, processes[i]);
+				}
 			}
 		}
-
+		
 		//TODO: THIS PART (REALTIME STUFF)
 		//first iterate through the realtime queue looking for the first process
 		//with enough free resources and fork into it	
 		//this is because realtime processes have priority over resources
 		
-		//TODO: FIGURE OUT WHAT TO DO FOR THIS FOR LOOP
+		
+		
 		//iterate through user job queue for the same reason
 		for(...)
 		{
+			tempProc = pop(4);
 			//if theres enough free resources
-			if (check_res(freeres, current process) == 1)
+			if (check_res(freeres, tempProc->process) == 1)
 			{
 				//if theres enough free memory it will be allocated
-				mem = alloc_mem(freeres, current process.mBytes);
+				memP = alloc_mem(freeres, tempProc->process.mBytes);
 				if(memP > 0)
 				{
 					//save the pointer to memory to process, and allocate resources
-					Current Proc.memPointer = memP;
-					alloc_res(freeres, current process);
+					tempProc->process.memPointer = memP;
+					alloc_res(freeres, tempProc->process);
 					//put the process in the correct queue based on its priority
 					//also fork it and immediately pause it
-					processes[i].pid = fork();
-					if (process[i].pid == 0)
+					tempProc->process.pid = fork();
+					if (tempProc->process.pid == 0)
 					{
 						//child
-						execv(sigtrap.c process[i].processorTime);
+						execv("./process %d", tempProc->process.processorTime);
 					}
 					else
 					{
 						//parent
 						//interrupt child here
-						signal(SIGINT, &process[i].pid);
+						signal(SIGINT, tempProc->&process.pid);
 					}
 				}
+			}
+			else
+			{
+				//resources couldn't be allocated
+			}
 		}
 		
 		//Now we can finally execute one time segment for the user jobs
 		//Pop the head in each of the priority 1-3 queues and enqueue that to the lower priority
-		proc P1 = Priority1.Pop();
-		proc P2 = Priority2.Pop();
-		proc P3 = priority3.Pop();
+		node_t * P1 = pop(1);
+		node_t * P2 = pop(2);
+		node_t * P3 = pop(3);
 		
 		//resume each of those popped heads, sleep for 1 second, then pause them again
-		signal(SIGCONT, &P1.pid);
-		signal(SIGCONT, &P2.pid);
-		signal(SIGCONT, &P3.pid);
-		P1.processorTime -= 1; P2.processorTime -= 1; P3.processorTime -= 1;
+		signal(SIGCONT, P1->process.pid);
+		signal(SIGCONT, P2->process.pid);
+		signal(SIGCONT, P3->process.pid);
+		P1->process.processorTime -= 1; P2->process.processorTime -= 1; P3->process.processorTime -= 1;
 		sleep(1);
-		signal(SIGINT, &P1.pid);
-		signal(SIGINT, &P2.pid);
-		signal(SIGINT, &P3.pid);
-		if (P1.processorTime > 0)
+		signal(SIGTSTP, P1->process.pid);
+		signal(SIGTSTP, P2->process.pid);
+		signal(SIGTSTP, P3->process.pid);
+		if (P1->process.processorTime > 0)
 		{
-			Priority2.Enqueue(P1);
-		}else{     }
-		//TODO: ADD KILLING OF PROCESSES IN ELSE STATEMENT
-		if (P2.processorTime > 0)
+			push(2, P1);
+		}else{ signal(SIGINT,P1->process.pid); run++;}
+		if (P2->process.processorTime > 0)
 		{
-			Priority3.enqueue(P2);
-		}else {     }
+			push(3, P2);
+		}else { signal(SIGINT,P2->process.pid); run++;}
 
-		if(P3.processorTime > 0)
+		if(P3->process.processorTime > 0)
 		{
-			Priority3.Enqueue(P3);
-		}else {     }
+			push(3, P3);
+		}else { signal(SIGINT,P3->process.pid); run++;}
+
 		
 
 		time++;
