63d90af892df54e55286557d0fcd6e594d69d13f
Bradon-Lodwick <bradonlodwick22@gmail.com>
2018-03-31 21:41:47 -0700

    ongoing testing continued again

--- labs/4/queue.c
+++ labs/4/queue.c
@@ -37,7 +37,9 @@ node_t * remove_node(node_t * head, node_t * node_to_remove)
 {
 	if (head == node_to_remove)
 	{
+		node_t * removed_node = *head;
 		head = head->next;
+		return removed_node;
 	}
 
 	else
@@ -46,11 +48,11 @@ node_t * remove_node(node_t * head, node_t * node_to_remove)
 		node_t * current = &head;
 		while (current->next != node_to_remove)
 		{
-			current = current->next;
 			if (current->next != NULL)
 			{
 				return NULL;
 			}
+			current = current->next;
 		}
 
 		// Node to be removed
